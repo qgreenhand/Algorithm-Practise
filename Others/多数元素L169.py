e4c26ddd07d5a59bd4635af493c6aba241256c7c
Qiu159487263199709 <3158874848@qq.com>
2020-06-17 10:40:26 +0800

    整理

--- Others/多数元素L169.py
+++ Others/多数元素L169.py
@@ -1,5 +1,8 @@
 from typing import List
 class Solution:
+    '''
+    给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+    '''
     def majorityElement(self, nums: List[int]) -> int:
         '''
         1. 先排序再弄
--- code1.py
+++ code1.py
@@ -1,37 +1,14 @@
 from typing import List
 class Solution:
 
-    def reversePairs(self, nums: List[int]) -> int:
-        '''
-        之前做过，采用类似归并排序的做法
-        '''
-        result=0
-        def do(nums):
-            nonlocal result
-            r=[]
-            if len(nums)==1:
-                return nums
-            mid=len(nums)//2
-            left=do(nums[:mid])
-            right=do(nums[mid:])
-            print(left ,right)
-            i=j=0
-            while i<len(left) and j<len(right):
-                if left[i]<=right[i]:
-                    r.append(left[i])
-                    i+=1
+    def reverseNumber(self, x,y) :
+        dp=[[0]*x for _ in range(y)]
+        for j in range(x):
+            for i in range(y):
+                if i==0 or j==0:
+                    dp[i][j]=1
                 else:
-                    r.append(right[j])
-                    result=result+len(left)-i
-                    j+=1
-            while i<len(left):
-                r.append(left[i])
-                i+=1
-            while j<len(right):
-                r.append(right[j])
-                j+=1
-            return r
-        do(nums)
-        return result
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return  dp[y-1][x-1]
 s=Solution()
-print(s.reversePairs([7,5,6,4]))
+print(s.reverseNumber(20,20))
