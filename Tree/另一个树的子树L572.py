bdb06c863bb299d49e6ddae21667064b6f967351
Qiu159487263199709 <3158874848@qq.com>
2020-06-20 10:49:01 +0800

    整理

--- Tree/另一个树的子树L572.py
+++ Tree/另一个树的子树L572.py
@@ -6,15 +6,16 @@ class TreeNode:
          self.right = None
 
 class Solution:
+    '''
+    给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树
+    '''
     def identical(self, node_a, node_b):  # 判定两棵树是否相同
         if not node_a and not node_b:  # 两个 node 都为空为 True
             return True
         if node_a is None or node_b is None:  # 一方空，一方不空，为False
             return False
         # 否则说明两个 node 都非空，那么如果两个树相等必须满足3个条件，即当前 node 的值相等，且各自左右子树也对应相等
-        return node_a.val == node_b.val and \
-               self.identical(node_a.left, node_b.left) and \
-               self.identical(node_a.right, node_b.right)
+        return node_a.val == node_b.val and self.identical(node_a.left, node_b.left) and self.identical(node_a.right, node_b.right)
 
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
         if not s:
