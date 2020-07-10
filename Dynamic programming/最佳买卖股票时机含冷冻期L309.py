f7ee811164048c1b913d4559ff70edc9fa0ac60e
Qiu159487263199709 <3158874848@qq.com>
2020-07-10 10:49:34 +0800

    add

--- Dynamic programming/最佳买卖股票时机含冷冻期L309.py
+++ Dynamic programming/最佳买卖股票时机含冷冻期L309.py
@@ -22,7 +22,7 @@ class Solution:
         for i in range(1, len(prices)):
 
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-            if i >= 2:
+            if i >= 3:
                 dp[i][1] = max(dp[i - 1][1], dp[i - 2][0] - prices[i])
             else:
                 dp[i][1] = max(dp[i - 1][1], dp[i][1])
