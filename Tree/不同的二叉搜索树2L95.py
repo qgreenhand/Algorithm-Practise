a5cf6237a8bb6cec82a62371e9045aa399768acb
Qiu159487263199709 <3158874848@qq.com>
2020-07-21 18:52:51 +0800

    add

---
+++ Tree/不同的二叉搜索树2L95.py
@@ -0,0 +1,36 @@
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    '''
+    给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+    '''
+
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        def generateTrees(start, end):
+            if start > end:
+                return [None, ]
+
+            allTrees = []
+            for i in range(start, end + 1):  # 枚举可行根节点
+                # 获得所有可行的左子树集合
+                leftTrees = generateTrees(start, i - 1)
+
+                # 获得所有可行的右子树集合
+                rightTrees = generateTrees(i + 1, end)
+
+                # 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+                for l in leftTrees:
+                    for r in rightTrees:
+                        currTree = TreeNode(i)
+                        currTree.left = l
+                        currTree.right = r
+                        allTrees.append(currTree)
+
+            return allTrees
+
+        return generateTrees(1, n) if n else []
