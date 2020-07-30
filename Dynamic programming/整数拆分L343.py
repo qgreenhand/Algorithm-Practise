fd9f607177bbd74cbfb9a22d473359a6bcb3d8e6
Honghao Qiu <3158874848@qq.com>
2020-07-30 22:29:10 +0800

    add

---
+++ Dynamic programming/整数拆分L343.py
@@ -0,0 +1,15 @@
+class Solution:
+    '''
+    给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积
+    '''
+
+    def integerBreak(self, n: int) -> int:
+        '''
+        感觉是比较简单的动态规划
+        '''
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
+
