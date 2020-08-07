b9bf0e2aac703946c7a9c833fdcf64a2ae571de0
Honghao Qiu <3158874848@qq.com>
2020-08-07 23:47:37 +0800

    add

---
+++ Tree/相同的树L100.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    '''
+    给定两个二叉树，编写一个函数来检验它们是否相同。
+    如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+    '''
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
