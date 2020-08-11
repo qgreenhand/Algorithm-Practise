68e9675d93be7475ab48b13cb479d869113921ab
Honghao Qiu <3158874848@qq.com>
2020-08-11 19:17:40 +0800

    add

---
+++ Others/被围绕的区域L130.py
@@ -0,0 +1,39 @@
+class Solution:
+    '''
+    给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+    找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+    '''
+
+    def solve(self, board: List[List[str]]) -> None:
+        '''
+
+        '''
+        if not board:
+            return
+
+        n, m = len(board), len(board[0])
+
+        def dfs(x, y):
+            if not 0 <= x < n or not 0 <= y < m or board[x][y] != 'O':
+                return
+
+            board[x][y] = "A"
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m - 1)
+
+        for i in range(m - 1):
+            dfs(0, i)
+            dfs(n - 1, i)
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "A":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
