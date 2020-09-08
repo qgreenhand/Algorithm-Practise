cf1e3de29638267c55924ee55f9ade155fbd0edc
Honghao Qiu <3158874848@qq.com>
2020-09-08 16:15:54 +0800

    review

--- Dynamic programming/最大正方形L221.py
+++ Dynamic programming/最大正方形L221.py
@@ -4,7 +4,7 @@ class Solution:
         '''
         在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积
         动态规划，dp数组里存以i，j为结尾的最大矩阵
-        dp[i,j]=min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
+        dp[i,j]=min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1
         '''
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return 0
