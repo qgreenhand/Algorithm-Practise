4863714ee27f0c3f72fa9d821dde8eed000529bc
Honghao Qiu <3158874848@qq.com>
2020-09-22 08:46:07 +0800

    ADD the todo, so i can review some time

---
+++ Tree/监控二叉树L968.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """
+    给定一个二叉树，我们在树的节点上安装摄像头。
+    节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+    计算监控树的所有节点所需的最小摄像头数量。
+
+    """
+    def minCameraCover(self, root: TreeNode) -> int:
+        """
+        TODO
+        主要有三种状态，
+        现在先留个TODO日后再做
+        :param root:
+        :return:
+        """
+        return -1
