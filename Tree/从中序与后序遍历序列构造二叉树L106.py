81b0eb900ae0a3e33b3828c7b4b8108b509b070b
Honghao Qiu <3158874848@qq.com>
2020-09-25 09:14:59 +0800

    构造二叉树

---
+++ Tree/从中序与后序遍历序列构造二叉树L106.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+from typing import List
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """
+    根据一棵树的中序遍历与后序遍历构造二叉树。
+    注意:
+    你可以假设树中没有重复的元素。
+    """
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        def helper(in_left, in_right):
+            """
+            由于是后序遍历顺序为左右根所以要先建右子树再建左子树
+            :param in_left:
+            :param in_right:
+            :return:
+            """
+            # 如果这里没有节点构造二叉树了，就结束
+            if in_left > in_right:
+                return None
+
+            # 选择 post_idx 位置的元素作为当前子树根节点
+            val = postorder.pop()
+            root = TreeNode(val)
+
+            # 根据 root 所在位置分成左右两棵子树
+            index = idx_map[val]
+
+            # 构造右子树
+            root.right = helper(index + 1, in_right)
+            # 构造左子树
+            root.left = helper(in_left, index - 1)
+            return root
+
+        # 建立（元素，下标）键值对的哈希表
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        return helper(0, len(inorder) - 1)
+
