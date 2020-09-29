3118db758a9205578cd8b7e9f799bbb6fe9a53d0
Honghao Qiu <3158874848@qq.com>
2020-09-29 09:54:00 +0800

    add

--- Tree/二叉树的后序遍历L145.py
+++ Tree/二叉树的后序遍历L145.py
@@ -6,6 +6,14 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    """
+    除了这个还有个取巧的方法。。。
+    可以直接将前序遍历的压栈顺序颠倒下然后结果逆序输出：
+    后序 ：左右中
+    前序 ：中左右
+    压栈顺序改变： 中右左
+    逆序输出： 左右中（后序）
+    """
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         def postorder(root):
             res = []
