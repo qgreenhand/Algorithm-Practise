534f5d3968f38b50066a3cabd0519e3a361e8a5e
Honghao Qiu <3158874848@qq.com>
2020-10-18 10:09:14 +0800

    add

--- BackTrack/N皇后L51.py
+++ BackTrack/N皇后L51.py
@@ -1,3 +1,52 @@
-"""
-TODO
-"""
+from typing import List
+class Solution:
+    """
+    n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+    """
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        """
+        主要是自己对于回溯算法不大熟
+        八皇后经典回朔算法diagonal表示从左上到右下列
+                        diagonal2表示左下到右上列
+                        columns储存列使用状态
+
+        :param n:
+        :return:
+        """
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def backtrack(row: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                #第row行开始对每个可能的列进行尝试
+                for i in range(n):
+                    if i in columns or row - i in diagonal1 or row + i in diagonal2:
+                        continue
+                    queens[row] = i
+                    columns.add(i)
+                    #两个斜线可以用 行加列  行减列分别标识
+                    diagonal1.add(row - i)
+                    diagonal2.add(row + i)
+                    backtrack(row + 1)
+                    columns.remove(i)
+                    diagonal1.remove(row - i)
+                    diagonal2.remove(row + i)
+
+        solutions = list()
+        queens = [-1] * n
+        columns = set()
+        diagonal1 = set()
+        diagonal2 = set()
+        row = ["."] * n
+        backtrack(0)
+        return solutions
+
