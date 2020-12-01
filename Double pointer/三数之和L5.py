807fa24b0a431d0eccfb430a58837c7de21374b8
Honghao Qiu <3158874848@qq.com>
2020-12-01 09:33:07 +0800

    review

--- Double pointer/三数之和L5.py
+++ Double pointer/三数之和L5.py
@@ -1,33 +1,35 @@
 from typing import List
+
+
 class Solution:
-    def threeSum(self, nums : List[int]) :
-        #三数之和，先排序再用双指针
+    def threeSum(self, nums: List[int]):
+        # 三数之和，先排序再用双指针
 
         nums.sort()
-        answers=[]
-        for i in range(0,len(nums)):
-            j=i+1
-            k=len(nums)-1
-            subnum=int                                          #subnum用以存储之前的num[j]防止出现重复元组
-            #由于排序后从小到大故如果nums[i]>0那么和也一定大于0
-            if nums[i]>0: break
-            #防止重复元组进入
-            if i>0 and nums[i]==nums[i-1]: continue
-            while j<k:
-                s=nums[i]+nums[j]+nums[k]
-                if s==0:
-                    if nums[j]!=subnum:
-                        answers.append([nums[i],nums[j],nums[k]])
-                    subnum=nums[j]
-                    j+=1
-                    k-=1
-                    #和大于0代表需减小所以k--
-                elif s>0:
-                    k-=1
+        answers = []
+        for i in range(0, len(nums)):
+            j = i + 1
+            k = len(nums) - 1
+            subnum = int  # subnum用以存储之前的num[j]防止出现重复元组
+            # 由于排序后从小到大故如果nums[i]>0那么和也一定大于0
+            if nums[i] > 0: break
+            # 防止重复元组进入
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == 0:
+                    if nums[j] != subnum:
+                        answers.append([nums[i], nums[j], nums[k]])
+                    subnum = nums[j]
+                    j += 1
+                    k -= 1
+                    # 和大于0代表需减小所以k--
+                elif s > 0:
+                    k -= 1
                 else:
-                    j+=1
+                    j += 1
         return answers
 
-s=Solution()
-print(s.threeSum([3,0,-2,-1,1,2]))
 
+s = Solution()
+print(s.threeSum([3, 0, -2, -1, 1, 2]))
--- Double pointer/最大水池容量L11.py
+++ Double pointer/最大水池容量L11.py
@@ -1,33 +1,35 @@
 class Solution:
-    def maxArea(self, height) :
-        '''
+    """
+    给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+    在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。
+    找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    """
+    def maxArea(self, height):
+        """
         能解决问题的原因是控制了变量
         在这个题目里面有两个变量一个是左右墙体较矮的那个一个是坐标轴长度
         这种方式使得坐标轴始终在减小每次舍去较矮的那个墙
         保证遍历完全不会有更大的可能没有遍历到
+        。。。
+        review 2020/12/01
         :param height:
         :return:
-        '''
-        #找最大水池容量“双指针法”
-        lenth=len(height)
-        i=0
-        j=lenth-1
-        marea=0
-        while j>i:
-            area=min(height[i],height[j])*(j-i)
-            if area>marea: marea=area
-            if height[i]>height[j]:
-                j-=1
+        """
+        # 找最大水池容量“双指针法”
+        lenth = len(height)
+        i = 0
+        j = lenth - 1
+        marea = 0
+        while j > i:
+            area = min(height[i], height[j]) * (j - i)
+            if area > marea: marea = area
+            if height[i] > height[j]:
+                j -= 1
             else:
-                i+=1
-
+                i += 1
 
         return marea
 
-s=Solution()
-print(s.maxArea([1,8,6,2,5,4,8,3,7]))
 
-
-
-
-
+s = Solution()
+print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
