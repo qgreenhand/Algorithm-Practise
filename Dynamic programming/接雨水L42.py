cc70ce61c6e6bf0f8ad1d03ddd290e6858f9683f
Honghao Qiu <3158874848@qq.com>
2020-12-28 18:02:43 +0800

    review

--- Dynamic programming/接雨水L42.py
+++ Dynamic programming/接雨水L42.py
@@ -2,6 +2,8 @@ from typing import List
 class Solution:
     """
     给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+    。。。。
+    2020.12.28 review
     """
     def trap(self, height: List[int]) -> int:
         # 动态规划思想遍历获取每一格左右的最高高度
