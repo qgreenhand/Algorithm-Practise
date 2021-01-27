3c130b505a61a74abbf48aff8cc3fc13849f2491
Honghao Qiu <3158874848@qq.com>
2021-01-27 10:58:53 +0800

    review

--- Others/跳跃游戏2L45.py
+++ Others/跳跃游戏2L45.py
@@ -1,13 +1,16 @@
 from typing import List
 class Solution:
-    '''
+    """
     说实话没大看懂，
     今年天太热刚拖得地实在不想看了
     看题解第三个比较好理解
-    '''
+    。。。
+    2021.01.27
+    看明白了
+    """
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        maxPos, end, step = 0, 0, 0
+        maxPos, end, step = 0, 0, 0        # maxPos表示当前可以达到的最大位置，end表示上次最大位置的边界
         for i in range(n - 1):
             if maxPos >= i:
                 maxPos = max(maxPos, i + nums[i])
--- Others/颜色分类L75.py
+++ Others/颜色分类L75.py
@@ -1,13 +1,13 @@
 from typing import List
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        '''
+        """
         荷兰三色旗问题解
         给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
 
         此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
 
-        '''
+        """
         # 对于所有 idx < p0 : nums[idx < p0] = 0
         # curr是当前考虑元素的下标
         p0 = curr = 0
