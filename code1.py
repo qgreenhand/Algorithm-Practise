14f88d1306f1a532e28c9cd320679189b040cd67
Honghao Qiu <3158874848@qq.com>
2021-02-06 23:07:02 +0800

    review

--- code1.py
+++ code1.py
@@ -1,21 +1,14 @@
-from ftplib import FTP
-import time,tarfile,os
-#连接ftp
-def ftpconnect(host,port, username, password):
-    ftp = FTP()
-    # 打开调试级别2，显示详细信息
-    # ftp.set_debuglevel(2)
-    ftp.connect(host, port)
-    ftp.login(username, password)
-    return ftp
 
-#从本地上传文件到ftp
-def uploadfile(ftp, remotepath, localpath):
-    bufsize = 1024
-    fp = open(localpath, 'rb')
-    ftp.mkd("./test")  # 新建远程目录
-    ftp.storbinary('STOR ' + remotepath, fp, bufsize)
-    ftp.set_debuglevel(0)
-    fp.close()
-ftp = ftpconnect("218.2.130.250",9091,"test","123")
-uploadfile(ftp,"./test/test4.py","./test4.py")
+def dfs(nums,k):
+    if k==1:
+        return max(nums[0],nums[-1])
+    left = nums.pop(0)
+    la =dfs(nums,k-1)
+    la += left
+    nums.insert(0,left)
+    right = nums.pop()
+    ra = dfs(nums,k-1)
+    ra+=right
+    nums.append(right)
+    return max(ra,la)
+print(dfs([2,2,2], k = 2))
